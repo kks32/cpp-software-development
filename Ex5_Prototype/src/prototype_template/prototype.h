5622d94a81f569b096840c4903852aa64b6456c8
Krishna Kumar <kks32@gmx.com>
2014-08-03 01:41:33 +0100

    Prototype

--- Ex5_Prototype/src/prototype_template/prototype.h
+++ Ex5_Prototype/src/prototype_template/prototype.h
@@ -1,26 +1,30 @@
+//fichier prototypetpl.h
 
 #ifndef PROTOTYPE_H
 #define PROTOTYPE_H
 
 #include <string>
+#include <memory>
 
-class Prototype
+template <class T> class Prototype
 {
-        public:
-        virtual ~Prototype();        
-        virtual Prototype* Clone() const  = 0;
+public:
+    virtual ~Prototype(){}        
+    virtual std::unique_ptr<T> Clone() const =0 ;
 };
 
-class MySQLManager: public Prototype
+class MySQLManager : public Prototype<MySQLManager>
 {
-std::string m_host,m_login,m_pass;
-std::string m_base,m_table;
-public:
-         MySQLManager(const std::string& host="",const std::string& login="",const std::string& pass="");
-         MySQLManager* Clone() const;
 
-         void Display() const;
-         void Set(const std::string& base="",const std::string& table=""); 
+    std::string m_host,m_login,m_pass;
+    std::string m_base,m_table;
+
+public:
+    MySQLManager(const std::string host="",const std::string login="",const std::string pass="");        
+        
+    void Display() const ;        
+    std::unique_ptr<MySQLManager> Clone() const ;
+    //  void Set(const std::string base="",const std::string table=""); 
 };
 
 #endif
