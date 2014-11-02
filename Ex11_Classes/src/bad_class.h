d44ce47f2046689d62521a1afcd8e3c37db41fcd
Krishna Kumar <kks32@gmx.com>
2014-11-02 18:23:29 +0000

    good and bad class updated

--- Ex11_Classes/src/bad_class.h
+++ Ex11_Classes/src/bad_class.h
@@ -1,21 +1,32 @@
+// Example from Herb Sutter's Exception C++
+
 #ifndef _COMPLEX_H_
 #define _COMPLEX_H_
 
 class Complex {
  public:
+  // Constructor
   Complex(double real, double imaginary = 0)
       : _real(real), _imaginary(imaginary) {}
+
+  // Addition operator
   void operator+(Complex other) {
     _real = _real + other._real;
     _imaginary = _imaginary + other._imaginary;
   }
+
+  // << operator
   void operator<<(ostream os) {
     os << "(" << _real << "," << _imaginary << ")";
   }
+
+  // Pre-Increament Operator
   Complex operator++() {
     ++_real;
     return *this;
   }
+
+  // Post-increament operator
   Complex operator++(int) {
     Complex temp = *this;
     ++_real;
