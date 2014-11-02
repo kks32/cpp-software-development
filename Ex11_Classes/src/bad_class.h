b1f35c302498a3708a2b89d0fcb3cbe5a0d786e1
Krishna Kumar <kks32@gmx.com>
2014-11-02 15:50:54 +0000

    formatted bad and good class

--- Ex11_Classes/src/bad_class.h
+++ Ex11_Classes/src/bad_class.h
@@ -1,30 +1,29 @@
-class Complex
-{
-public:
-Complex( double real, double imaginary = 0 )
-: _real(real), _imaginary(imaginary)
-{
-}
-void operator+ ( Complex other )
-{
-_real = _real + other._real;
-_imaginary = _imaginary + other._imaginary;
-}
-void operator<<( ostream os )
-{
-os << "(" << _real << "," << _imaginary << ")";
-}
-Complex operator++()
-{
-++_real;
-return *this;
-}
-Complex operator++( int )
-{
-Complex temp = *this;
-++_real;
-return temp;
-}
-private:
-double _real, _imaginary;
+#ifndef _COMPLEX_H_
+#define _COMPLEX_H_
+
+class Complex {
+ public:
+  Complex(double real, double imaginary = 0)
+      : _real(real), _imaginary(imaginary) {}
+  void operator+(Complex other) {
+    _real = _real + other._real;
+    _imaginary = _imaginary + other._imaginary;
+  }
+  void operator<<(ostream os) {
+    os << "(" << _real << "," << _imaginary << ")";
+  }
+  Complex operator++() {
+    ++_real;
+    return *this;
+  }
+  Complex operator++(int) {
+    Complex temp = *this;
+    ++_real;
+    return temp;
+  }
+
+ private:
+  double _real, _imaginary;
 };
+
+#endif  // _COMPLEX_H_
--- Ex11_Classes/src/good_class.h
+++ Ex11_Classes/src/good_class.h
@@ -1,41 +1,36 @@
-class Complex
-{
-public:
-explicit Complex( double real, double imaginary = 0 )
-: real_(real), imaginary_(imaginary)
-{
-}
-Complex& operator+=( const Complex& other )
-{
-real_ += other.real_;
-imaginary_ += other.imaginary_;
-return *this;
-}
-Complex& operator++()
-{
-++real_;
-return *this;
-}
-const Complex operator++( int )
-{
-Complex temp( *this );
-++*this;
-return temp;
-}
-ostream& Print( ostream& os ) const
-{
-return os << "(" << real_ << "," << imaginary_ << ")";
-}
-private:
-double real_, imaginary_;
+#ifndef _COMPLEX_H_
+#define _COMPLEX_H_
+
+class Complex {
+ public:
+  explicit Complex(double real, double imaginary = 0)
+      : real_(real), imaginary_(imaginary) {}
+  Complex& operator+=(const Complex& other) {
+    real_ += other.real_;
+    imaginary_ += other.imaginary_;
+    return *this;
+  }
+  Complex& operator++() {
+    ++real_;
+    return *this;
+  }
+  const Complex operator++(int) {
+    Complex temp(*this);
+    ++*this;
+    return temp;
+  }
+  ostream& Print(ostream& os) const {
+    return os << "(" << real_ << "," << imaginary_ << ")";
+  }
+
+ private:
+  double real_, imaginary_;
 };
-const Complex operator+( const Complex& lhs, const Complex& rhs )
-{
-Complex ret( lhs );
-ret += rhs;
-return ret;
+const Complex operator+(const Complex& lhs, const Complex& rhs) {
+  Complex ret(lhs);
+  ret += rhs;
+  return ret;
 }
-ostream& operator<<( ostream& os, const Complex& c )
-{
-return c.Print(os);
-}
+ostream& operator<<(ostream& os, const Complex& c) { return c.Print(os); }
+
+#endif  // _COMPLEX_H_
