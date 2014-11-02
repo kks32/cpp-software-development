18568e6bf0a2a96c8519e068ce07fb054306270b
Krishna Kumar <skrishnakumar001@gmail.com>
2014-11-02 15:47:33 +0000

    Class mechanics

---
+++ Ex11_Classes/src/bad_class.h
@@ -0,0 +1,30 @@
+class Complex
+{
+public:
+Complex( double real, double imaginary = 0 )
+: _real(real), _imaginary(imaginary)
+{
+}
+void operator+ ( Complex other )
+{
+_real = _real + other._real;
+_imaginary = _imaginary + other._imaginary;
+}
+void operator<<( ostream os )
+{
+os << "(" << _real << "," << _imaginary << ")";
+}
+Complex operator++()
+{
+++_real;
+return *this;
+}
+Complex operator++( int )
+{
+Complex temp = *this;
+++_real;
+return temp;
+}
+private:
+double _real, _imaginary;
+};
