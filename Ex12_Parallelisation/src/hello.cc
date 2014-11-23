c2b40a071fbf1d28f2770e33cc5d6f02f7ee546a
Krishna Kumar <kks32@gmx.com>
2014-11-23 10:40:03 +0000

    Hello world

--- Ex12_Parallelisation/src/hello.cc
+++ Ex12_Parallelisation/src/hello.cc
@@ -1,21 +1,20 @@
 #include <omp.h>
-#include <iostream>
+#include <cstdio>
 
 int main(int argc, char* argv[]) {
   int nthreads, tid;
 
 // Fork a team of threads giving them their own copies of variables
-  std::cout << std::endl;
 #pragma omp parallel private(nthreads, tid)
   {
     // Obtain thread number
     tid = omp_get_thread_num();
-    std::cout << "Hello World from thread = " << tid << std::endl;
+    printf("Hello World from thread = %d\n", tid);
 
     // Only master thread does this
     if (tid == 0) {
       nthreads = omp_get_num_threads();
-      std::cout << "Number of threads = " << nthreads << std::endl;
+      printf("Number of threads = %d\n", nthreads);
     }
 
   }  // All threads join master thread and disband
