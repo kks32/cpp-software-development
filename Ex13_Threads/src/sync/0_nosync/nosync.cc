1f8c25ca2a0fd87428be4ef9f3ad0a6637fc9de0
Krishna Kumar <kks32@gmx.com>
2014-12-14 14:31:49 +0000

    Semaphore -librarian example

--- Ex13_Threads/src/sync/0_nosync/nosync.cc
+++ Ex13_Threads/src/sync/0_nosync/nosync.cc
@@ -18,7 +18,7 @@ int main() {
   Counter counter;
 
   std::vector<std::thread> threads;
-  for (int i = 0; i < 5; ++i) {
+  for (int i = 0; i < 500; ++i) {
     threads.push_back(std::thread([&counter]() {
       for (int i = 0; i < 100; ++i) {
         counter.increment();
