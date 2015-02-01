0c494676d0fca62196040f17bc56ea18466cd37e
Krishna Kumar <skrishnakumar001@gmail.com>
2015-02-01 17:29:37 +0000

    nested functions

---
+++ Ex15_Functors/src/nested_complete_solution.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class f {
+  int retval; // f's "return value"
+  int j;
+  int g( int k ) { return j + k; };
+
+ public:
+   f( int i ) : j( i*2 ) { // original function, now a constructor
+   j += 4;
+   retval = g( 3 );
+  }
+  operator int() const { // returning the result
+    return retval;
+  }
+
+};
+
+int main() {
+  std::cout << "Value of f(5) is : " << f(5) << std::endl;
+}
