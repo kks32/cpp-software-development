c35f70ea4bc7677140e44642be31315219ecceee
Krishna Kumar <kks32@gmx.com>
2015-02-14 16:18:41 +0000

    Copy constructor example

--- Ex16_Rvalue_Move_Semantics/src/copyconstructor.cc
+++ Ex16_Rvalue_Move_Semantics/src/copyconstructor.cc
@@ -1,4 +1,4 @@
-// This is an example with default copy constructor
+// This is an example with user-defined copy constructor
 #include <iostream>
 #include <string>
 
@@ -8,17 +8,28 @@ class Widget {
 
  public:
   Widget() { ptr = &x; };
+  // user-defined copy constructor
+  Widget(const Widget& w) : x{w.x} { ptr = &x; };
+  // user-defined assignment operator
+  Widget& operator=(const Widget& w) {
+    x = w.x;
+    ptr = &x;
+    return *this;
+  };
   int xvalue() { return *ptr; };
   void xvalue(const int newx) { x = newx; };
 };
+
 int main() {
   Widget w1;
   std::cout << "Widget W1 has a value of : " << w1.xvalue() << std::endl;
 
-  Widget w2{w1};  // default copy constructor is called
+  Widget w2{w1};  // user-defined copy constructor
+  // Widget w2 = w1; // user-defined assignment operator
+
   std::cout << "Widget W2 has a value of : " << w2.xvalue() << std::endl;
 
-  w1.xvalue(5); // Value of w1.x is changed to 5
+  w1.xvalue(5);  // Value of w1.x is changed to 5
   std::cout << "Widget W1 has a value of : " << w1.xvalue() << std::endl;
   std::cout << "Widget W2 has a value of : " << w2.xvalue() << std::endl;
 }
--- Ex16_Rvalue_Move_Semantics/src/copyconstructor_bad.cc
+++ Ex16_Rvalue_Move_Semantics/src/copyconstructor_bad.cc
@@ -16,6 +16,8 @@ int main() {
   std::cout << "Widget W1 has a value of : " << w1.xvalue() << std::endl;
 
   Widget w2{w1};  // default copy constructor is called
+  // Widget w2 = w1; // default assignment operator
+  
   std::cout << "Widget W2 has a value of : " << w2.xvalue() << std::endl;
 
   w1.xvalue(5); // Value of w1.x is changed to 5
