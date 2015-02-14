6697ccd3ba7bde68834225ca717ce8f4595b2254
Krishna Kumar <kks32@gmx.com>
2015-02-14 16:43:09 +0000

    Copy constructor

--- Ex16_Rvalue_Move_Semantics/src/copyconstructor_bad.cc
+++ Ex16_Rvalue_Move_Semantics/src/copyconstructor_bad.cc
@@ -11,16 +11,17 @@ class Widget {
   int xvalue() { return *ptr; };
   void xvalue(const int newx) { x = newx; };
 };
+
 int main() {
   Widget w1;
   std::cout << "Widget W1 has a value of : " << w1.xvalue() << std::endl;
 
   Widget w2{w1};  // default copy constructor is called
   // Widget w2 = w1; // default assignment operator
-  
+
   std::cout << "Widget W2 has a value of : " << w2.xvalue() << std::endl;
 
-  w1.xvalue(5); // Value of w1.x is changed to 5
+  w1.xvalue(5);  // Value of w1.x is changed to 5
   std::cout << "Widget W1 has a value of : " << w1.xvalue() << std::endl;
   std::cout << "Widget W2 has a value of : " << w2.xvalue() << std::endl;
 }
