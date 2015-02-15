6d1f2fd8f4adb3e7cd0104a71be3ebcae24c319d
Krishna Kumar <kks32@gmx.com>
2015-02-15 15:38:34 +0000

    Ex16 Perfect forwarding

--- Ex16_Rvalue_Move_Semantics/src/myvector_move.cc
+++ Ex16_Rvalue_Move_Semantics/src/myvector_move.cc
@@ -24,10 +24,7 @@ class Myvector {
 
   // Assignment & move operator
   Myvector<T>& operator=(Myvector<T> newvector) {
-    size = newvector.size;
-    elems = new T[newvector.size];
-    for (int i = 0; i < newvector.get_size(); ++i)
-      elems[i] = newvector.elems[i];
+    swap(newvector);
     return *this;
   }
 
@@ -67,7 +64,7 @@ int main() {
   Myvector<double> v{10};
   std::cout << "Elements in v" << std::endl;
   for (const auto& x : v) std::cout << x << std::endl;
-  Myvector<double> v2 = std::move(v);
+  Myvector<double> v2 = std::move(v); // try just passing 'v' as well v2 = v;
   std::cout << "Elements in v2" << std::endl;
   for (const auto& x : v2) std::cout << x << std::endl;
   std::cout << "How about the size of v :" << v.get_size() << std::endl;
