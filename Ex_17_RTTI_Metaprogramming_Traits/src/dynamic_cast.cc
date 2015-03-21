4987f6abd1d360e9917f44455d77f3499f0a0bd5
Krishna Kumar <kks32@gmx.com>
2015-03-21 18:14:25 +0000

    Dynamic casting

---
+++ Ex_17_RTTI_Metaprogramming_Traits/src/dynamic_cast.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+struct BaseA {
+  virtual void print() { std::cout << "Base Class A" << std::endl; }
+};
+
+struct DerivedB : BaseA {
+
+  virtual void print() { std::cout << "Derived Class B" << std::endl; }
+};
+
+struct DerivedC : BaseA {
+
+  virtual void print() { std::cout << "Derived Class C" << std::endl; }
+};
+
+void dynamic_cast_function(BaseA* arg) {
+
+  DerivedB* bp = dynamic_cast<DerivedB*>(arg);
+
+  DerivedC* cp = dynamic_cast<DerivedC*>(arg);
+
+  if (bp)
+    bp->print();
+
+  else if (cp)
+    cp->print();
+
+  else
+    arg->print();
+};
+
+int main() {
+
+  BaseA aobj;
+
+  DerivedC cobj;
+
+  BaseA* ptr = &aobj;
+
+  decltype(ptr) ptr2 = &cobj; //BaseA* ptr
+
+  dynamic_cast_function(ptr);
+
+  dynamic_cast_function(ptr2);
+}
